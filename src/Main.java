19e4c7c9b88c5a4f9e28bc70b2ee29b8b01ca3fb
B owie <usazevedo.dev@gmail.com>
2023-09-26 21:47:35 -0300

    Simmmmm

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("VocÊ");
         System.out.println("ççççç são");
         System.out.println("E não é que esta dando certo o commit pelo Intellij");
+        System.out.println("Mais uma vez parece que foi");
     }
 
 }
